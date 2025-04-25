d6aa60ec6e430b0a3dc5df738faa01fa2d6bae0b
nemofromdreamland <rafafrassetto@outlook.com>
2025-04-25 11:04:41 -0300

    Criando objetos do usuario

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,7 +1,23 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"CRUD-GO/src/configuration/rest_err"
+	"CRUD-GO/src/controller/model/request"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context) {
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect filds, error=%s\n", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
 
 }
