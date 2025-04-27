d7dfcc1b9e59b1bd838aa266ca1c284096149976
nemofromdreamland <rafafrassetto@outlook.com>
2025-04-27 10:38:34 -0300

    criando o delete user repository e melhorando o create user

--- src/model/repository/delete_user_repository.go
+++ src/model/repository/delete_user_repository.go
@@ -1,1 +1,41 @@
 package repository
+
+import (
+	"CRUD-GO/src/configuration/logger"
+	"CRUD-GO/src/configuration/rest_err"
+	"context"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.uber.org/zap"
+)
+
+func (ur *userRepository) DeleteUser(
+	userId string,
+) *rest_err.RestErr {
+	logger.Info("Init deleteUser repository",
+		zap.String("journey", "deleteUser"))
+
+	collection_name := os.Getenv(MONGODB_USER_DB)
+	collection := ur.databaseConnection.Collection(collection_name)
+
+	userIdHex, _ := primitive.ObjectIDFromHex(userId)
+
+	filter := bson.D{{Key: "_id", Value: userIdHex}}
+
+	_, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		logger.Error("Error trying to delete user",
+			err,
+			zap.String("journey", "deleteUser"))
+		return rest_err.NewInternalServerError(err.Error())
+	}
+
+	logger.Info(
+		"deleteUser repository executed successfully",
+		zap.String("userId", userId),
+		zap.String("journey", "deleteUser"))
+
+	return nil
+}
