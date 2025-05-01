22339c884a5a144e3726eae304739fd4dc9ddb02
nemofromdreamland <rafafrassetto@outlook.com>
2025-05-01 12:15:03 -0300

    Adicionando testes unitários para o createUserServices da aplicação

---
+++ src/model/service/create_user_test.go
@@ -0,0 +1,76 @@
+package service
+
+import (
+	"CRUD-GO/src/configuration/rest_err"
+	"CRUD-GO/src/model"
+	"CRUD-GO/src/tests/mocks"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func TestUserDomainService_CreateUserServices(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repository := mocks.NewMockUserRepository(ctrl)
+	service := NewUserDomainService(repository)
+
+	t.Run("when_user_already_exists_returns_error", func(t *testing.T) {
+		id := primitive.NewObjectID().Hex()
+
+		userDomain := model.NewUserDomain("test@test.com", "test", "test", 50)
+		userDomain.SetID(id)
+
+		repository.EXPECT().FindUserByEmail(userDomain.GetEmail()).Return(userDomain, nil)
+
+		user, err := service.CreateUserServices(userDomain)
+
+		assert.Nil(t, user)
+		assert.NotNil(t, err)
+		assert.EqualValues(t, err.Message, "Email is already registered in another account")
+	})
+
+	t.Run("when_user_is_not_registered_returns_error", func(t *testing.T) {
+		id := primitive.NewObjectID().Hex()
+
+		userDomain := model.NewUserDomain("test@test.com", "test", "test", 50)
+		userDomain.SetID(id)
+
+		repository.EXPECT().FindUserByEmail(userDomain.GetEmail()).Return(
+			nil, nil)
+
+		repository.EXPECT().CreateUser(userDomain).Return(
+			nil, rest_err.NewInternalServerError("error trying to create user"))
+
+		user, err := service.CreateUserServices(userDomain)
+
+		assert.Nil(t, user)
+		assert.NotNil(t, err)
+		assert.EqualValues(t, err.Message, "error trying to create user")
+	})
+
+	t.Run("when_user_is_not_registered_returns_success", func(t *testing.T) {
+		id := primitive.NewObjectID().Hex()
+
+		userDomain := model.NewUserDomain("test@test.com", "test", "test", 50)
+		userDomain.SetID(id)
+
+		repository.EXPECT().FindUserByEmail(userDomain.GetEmail()).Return(
+			nil, nil)
+
+		repository.EXPECT().CreateUser(userDomain).Return(
+			userDomain, nil)
+
+		user, err := service.CreateUserServices(userDomain)
+
+		assert.Nil(t, err)
+		assert.EqualValues(t, user.GetName(), userDomain.GetName())
+		assert.EqualValues(t, user.GetEmail(), userDomain.GetEmail())
+		assert.EqualValues(t, user.GetAge(), userDomain.GetAge())
+		assert.EqualValues(t, user.GetID(), userDomain.GetID())
+		assert.EqualValues(t, user.GetPassword(), userDomain.GetPassword())
+	})
+}
