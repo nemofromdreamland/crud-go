60c9ad03fd08ac81c729e4f301ee1549a2b14861
nemofromdreamland <rafafrassetto@outlook.com>
2025-05-02 16:08:15 -0300

    criando teste unitario para delete_user_test.go em controller

---
+++ src/controller/delete_user_test.go
@@ -0,0 +1,82 @@
+package controller
+
+import (
+	"CRUD-GO/src/configuration/rest_err"
+	"CRUD-GO/src/tests/mocks"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func TestUserControllerInterface_DeleteUser(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	service := mocks.NewMockUserDomainService(ctrl)
+	controller := NewUserControllerInterface(service)
+
+	t.Run("userId_is_invalid_returns_error", func(t *testing.T) {
+		recorder := httptest.NewRecorder()
+		context := GetTestGinContext(recorder)
+
+		param := []gin.Param{
+			{
+				Key:   "userId",
+				Value: "teste",
+			},
+		}
+
+		MakeRequest(context, param, url.Values{}, "DELETE", nil)
+		controller.DeleteUser(context)
+
+		assert.EqualValues(t, http.StatusBadRequest, recorder.Code)
+	})
+
+	t.Run("id_is_valid_service_returns_error", func(t *testing.T) {
+		recorder := httptest.NewRecorder()
+		context := GetTestGinContext(recorder)
+		id := primitive.NewObjectID().Hex()
+
+		param := []gin.Param{
+			{
+				Key:   "userId",
+				Value: id,
+			},
+		}
+
+		service.EXPECT().DeleteUser(id).Return(
+			rest_err.NewInternalServerError("error test"))
+
+		MakeRequest(context, param, url.Values{}, "DELETE", nil)
+		controller.DeleteUser(context)
+
+		assert.EqualValues(t, http.StatusInternalServerError, recorder.Code)
+	})
+
+	t.Run("id_is_valid_service_returns_success", func(t *testing.T) {
+		recorder := httptest.NewRecorder()
+		context := GetTestGinContext(recorder)
+		id := primitive.NewObjectID().Hex()
+
+		param := []gin.Param{
+			{
+				Key:   "userId",
+				Value: id,
+			},
+		}
+
+		service.EXPECT().DeleteUser(id).Return(nil)
+
+		MakeRequest(context, param, url.Values{}, "DELETE", nil)
+		controller.DeleteUser(context)
+
+		assert.EqualValues(t, http.StatusOK, recorder.Code)
+	})
+
+}
